710d229b2265f4a419897598798c3fe7c5874f82
Gautham Asok <gauthamasok.dev@gmail.com>
2020-01-19 22:55:51 +0530

    updated code

--- search.php
+++ search.php
@@ -21,10 +21,13 @@ if (mysqli_num_rows($result) > 0) {
 		<div class="card col-xl-7 col-lg-11 col-md-11 col-sm-11 mx-auto" style="border-radius: 25px;">
 		<div class="card-body">
 		<!-- card body starts -->
-		<div class="text-center pb-3"><kbd><i class="fas fa-credit-card"></i>&nbsp;&nbsp;<i>'.$row['vip_card_no'].'</i></kbd></div>
+		<div class="pb-3 clearfix">
+		<span class="float-left"><kbd><i class="fas fa-credit-card"></i>&nbsp;&nbsp;<i>'.$row['vip_card_no'].'</i></kbd></span>
+		<span class="float-right"><a href="#!" data-toggle="modal" data-target="#myHis">Full Transaction History</a></span>
+		</div>
 		<div class="d-flex justify-content-between py-2">
-		<div class=""><h3><i class="fas fa-user-circle"></i>&nbsp;'.$row['name'].'</h3></div>
-		<div class=""><h3 class="bg-warning brd px-4">&nbsp;<i class="fas fa-rupee-sign"></i>&nbsp;'.$row['current_value'].'&nbsp;</h3></div>
+		<div class="text-left"><h3><i class="fas fa-user-circle py-2"></i>&nbsp;'.$row['name'].'</h3></div>
+		<div class="text-right"><h3 class="bg-warning brd px-4 py-1">&nbsp;<i class="fas fa-rupee-sign"></i>&nbsp;'.$row['current_value'].'&nbsp;</h3></div>
 		</div><hr class="">
 		<div class="row p-2">
 		<div class="col">
@@ -47,57 +50,107 @@ if (mysqli_num_rows($result) > 0) {
 	}
 
 	echo '
-		<!-- The Modal -->
-		<div class="modal" id="myAdd" data-backdrop="static">
-		<div class="modal-dialog modal-dialog-centered">
-		<div class="modal-content">
+	<!-- The Modal -->
+	<div class="modal fade" id="myAdd" data-backdrop="static">
+	<div class="modal-dialog modal-dialog-centered">
+	<div class="modal-content">
+	<form>
+	<!-- Modal Header -->
+	<div class="modal-header">
+	<h4 class="modal-title">Add Amount</h4>
+	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	</div>
 
-		<!-- Modal Header -->
-		<div class="modal-header">
-		<h4 class="modal-title">Add Heading</h4>
-		<button type="button" class="close" data-dismiss="modal">&times;</button>
-		</div>
+	<!-- Modal body -->
+	<div class="modal-body">
+	<div class="input-group mb-3">
+	<div class="input-group-prepend">
+	<span class="input-group-text">Amount:</span>
+	</div>
+	<input type="text" class="form-control" id="adamt" required>
+	</div>
+	</div>
 
-		<!-- Modal body -->
-		<div class="modal-body">
-		Add body..
-		</div>
+	<!-- Modal footer -->
+	<div class="modal-footer">
+	<button type="submit" class="btn btn-success brd px-5" id="adsub"><i class="fas fa-plus-square"></i>&nbsp;<span class="font-weight-bold">Add</span></button>
+	</div>
+	</form>
+	</div>
+	</div>
+	</div>
+	';
+	echo '
+	<!-- The Modal -->
+	<div class="modal fade" id="myMin" data-backdrop="static">
+	<div class="modal-dialog modal-dialog-centered">
+	<div class="modal-content">
+	<form>
+	<!-- Modal Header -->
+	<div class="modal-header">
+	<h4 class="modal-title">VIP Swipe</h4>
+	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	</div>
 
-		<!-- Modal footer -->
-		<div class="modal-footer">
-		<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-		</div>
+	<!-- Modal body -->
+	<div class="modal-body">
+	<div class="input-group mb-3">
+	<div class="input-group-prepend">
+	<span class="input-group-text">Bill ID:</span>
+	</div>
+	<input type="text" class="form-control" id="abid" required>
+	</div>
 
-		</div>
-		</div>
-		</div>
-		';
-		echo '
-		<!-- The Modal -->
-		<div class="modal" id="myMin" data-backdrop="static">
-		<div class="modal-dialog modal-dialog-centered">
-		<div class="modal-content">
+	<div class="input-group mb-3">
+	<div class="input-group-prepend">
+	<span class="input-group-text">Bill Amount:</span>
+	</div>
+	<input type="text" class="form-control" id="abill" required>
+	</div>
 
-		<!-- Modal Header -->
-		<div class="modal-header">
-		<h4 class="modal-title">Minus</h4>
-		<button type="button" class="close" data-dismiss="modal">&times;</button>
-		</div>
+	<div class="input-group mb-3">
+	<div class="input-group-prepend">
+	<span class="input-group-text">Card Amount:</span>
+	</div>
+	<input type="text" class="form-control" id="acard" required>
+	</div>
+	</div>
 
-		<!-- Modal body -->
-		<div class="modal-body">
-		Minus body..
-		</div>
+	<!-- Modal footer -->
+	<div class="modal-footer">
+	<button type="submit" class="btn btn-success brd px-5" id="usub"><i class="fas fa-minus-square"></i>&nbsp;<span class="font-weight-bold">Use</span></button>
+	</div>
+	</form>
+	</div>
+	</div>
+	</div>
+	';
+	echo '
+	<!-- The Modal -->
+	<div class="modal fade" id="myHis" data-backdrop="static">
+	<div class="modal-dialog">
+	<div class="modal-content">
 
-		<!-- Modal footer -->
-		<div class="modal-footer">
-		<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-		</div>
+	<!-- Modal Header -->
+	<div class="modal-header">
+	<h4 class="modal-title">Transaction History</h4>
+	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	</div>
 
-		</div>
-		</div>
-		</div>
-		';
+	<!-- Modal body -->
+	<div class="modal-body">
+	Transaction body..
+	</div>
+
+	<!-- Modal footer -->
+	<div class="modal-footer">
+	<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+	</div>
+
+	</div>
+	</div>
+	</div>
+	';
 
 } else {
 	echo "<div class='bg-white'>No Entry Dude!</div>";
